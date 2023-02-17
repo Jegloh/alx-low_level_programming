4efa002511f772cf3a8066367d477273cc55418b
Jegloh <jesseegloh@gmail.com>
2023-02-16 23:06:11 -0800

    C quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a sentence to the standard error.
- * Return: 1 (Success)
- */
+*main - Entry
+*Return: 1 (success)
+*/
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar,
-	2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
